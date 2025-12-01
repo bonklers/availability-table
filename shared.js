d8e4e596184e747164fae76f217013b2e7973985
Fernando <Fernando@gmail.com>
2025-11-30 22:28:20 -0500

    update version

--- shared.js
+++ shared.js
@@ -1,7 +1,7 @@
 /* =======================
    GLOBAL VERSION STRING
 ======================= */
-const VERSION = "v0.18";
+const VERSION = "v0.19";
 
 /* =======================
    APPLY VERSION TO TITLE
